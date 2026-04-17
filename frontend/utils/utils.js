f7b8163c1a1ed3b2f87572f5fd424f783240bbf8
04amanrajj <04.aman.raj@gmail.com>
2026-04-17 18:07:38 +0530

    feat: reusable functions

---
+++ frontend/utils/utils.js
@@ -0,0 +1,163 @@
+// let baseURL = "http://localhost:4500";
+let baseURL = "../restaurant.json";
+let currUser = JSON.parse(localStorage.getItem("user"));
+
+const tostTopEnd = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: false,
+});
+
+async function navbar() {
+  try {
+    const response = await axios.get(baseURL);
+    const restaurent = response.data.data.restaurantDetails;
+    const title = document.querySelector("title");
+    title.textContent += ` | ${restaurent.name}`;
+    const navbar = document.querySelector("header");
+    navbar.innerHTML = `
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container-fluid">
+          <!-- Navbar Brand -->
+          <a class="navbar-brand" href="/index.html"> ${restaurent.name} </a>
+          
+          <!-- Navbar Toggler -->
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <!-- Navbar Links -->
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ms-auto">
+              <!-- Profile Dropdown -->
+              <li class="nav-item dropdown profile-dropdown">
+                <a class="dropdown-item" href="/pages/admin.html">
+                  Admin
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>`;
+
+    // theme switch
+    let thememode = localStorage.getItem("theme") || "dark";
+    document.documentElement.dataset.theme = thememode;
+
+  } catch (error) {
+    console.error(error);
+    tostTopEnd.fire({
+      icon: "error",
+      title: error.message,
+    });
+  }
+}
+
+async function cover_page() {
+  try {
+    const response = await axios.get(baseURL);
+    const restaurent = response.data.data.restaurantDetails;
+    const coverPage = document.querySelector(".cover-page");
+    coverPage.innerHTML += `<h1>${restaurent.name}<span class="restaurant-tagline">${restaurent.tagline}</span></h1>`;
+  } catch (error) {
+    console.error(error);
+    tostTopEnd.fire({
+      icon: "error",
+      title: error.message,
+    });
+  }
+}
+
+async function page_footer() {
+  try {
+    const response = await axios.get(baseURL);
+    const restaurent = response.data.data.restaurantDetails;
+    const footer = document.querySelector("footer");
+    footer.innerHTML = `
+      <div class="container">
+        <div class="row mt-4">
+          <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+            <h5 class="text-uppercase mb-4">About restaurent</h5>
+            <p>
+              We serve a variety of cuisines, including North Indian, Punjabi, and fast food, made with authentic flavors.
+              Our meals are prepared with a homemade touch to give you the comfort of home-cooked food.
+            </p>
+            <p>
+              Enjoy your favorite dishes delivered straight to your doorstep with our quick home delivery service.
+            </p>
+            <div class="mt-4">
+              <a href="${
+                restaurent.socialMedia.facebook || "#"
+              }" type="button" class="btn btn-floating btn-light btn-lg"
+                ><i class="fab fa-facebook-f"></i
+              ></a>
+              <a href="${
+                restaurent.socialMedia.twitter || "#"
+              }" type="button" class="btn btn-floating btn-light btn-lg"
+                ><i class="fab fa-twitter"></i
+              ></a>
+              <a href="${
+                restaurent.socialMedia.instagram || "#"
+              }" type="button" class="btn btn-floating btn-light btn-lg"
+                ><i class="fab fa-instagram"></i
+              ></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+            <ul class="fa-ul" style="margin-left: 1.65em">
+              <li class="mb-3">
+                <i class="fas fa-home"></i>
+                <span class="ms-2">${restaurent.address.line1}</span>
+              </li>
+              <li class="mb-3">
+                <i class="fas fa-location-dot"></i>
+                <span class="ms-2">${restaurent.address.city}</span>
+              </li>
+              <li class="mb-3">
+                <i class="fas fa-map"></i>
+                <span class="ms-2">${restaurent.address.state}</span>
+              </li>
+              <li class="mb-3">
+                <i class="fas fa-envelope"></i>
+                <span class="ms-2">${restaurent.contact.email}</span>
+              </li>
+              <li class="mb-3">
+                <i class="fas fa-phone"></i>
+                <span class="ms-2">${restaurent.contact.phone}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 202 Copyright:
+        <a class="text-white" href="#" style="text-decoration:none"
+          >${restaurent.name}</a
+        >
+      </div>`;
+  } catch (error) {
+    console.error(error);
+    tostTopEnd.fire({
+      icon: "error",
+      title: error.message,
+    });
+  }
+}
+
+export {
+  baseURL,
+  page_footer,
+  cover_page,
+  navbar,
+  tostTopEnd,
+};
